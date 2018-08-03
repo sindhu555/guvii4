d2fa47bcb86f47e44c3ad01752cef60a2b367bcf
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-03 13:58:20 +0530

    Update binary.java

--- binary.java
+++ binary.java
@@ -1,4 +1,8 @@
-import.java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception

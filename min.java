061f4702e51a32d046e5e1c99f7ed33fa59925ac
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-03 14:23:53 +0530

    Update min.java

--- min.java
+++ min.java
@@ -9,23 +9,12 @@ class Ideone
 	{
 		// your code goes here
 				Scanner sc=new Scanner(System.in);
-		int[] a=new int[10];
-		for(int i=0;i<10;i++)
-		{
-			a[i]=sc.nextInt();
-		}
-		for(int i=0;i<10;i++)
-		{
-			for(int j=0;j<10-i-1;j++)
-			{
-				if(a[j]>a[j+1])
-				{
-					int temp=a[j];
-					a[j]=a[j+1];
-					a[j+1]=temp;
-				}
-			}
-		}
-		System.out.println(a[0]);
+                        int n=sc.nextInt();
+                        int m=sc.nextInt();
+                        int sum=n+m;
+                        if(sum%2==0)
+                        	System.out.println("even");
+                        else
+                        	System.out.println("odd");
 	}
-}
+}	

ce64ded32b50ad91ba0c0857083ecd1be17c7a0c
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-13 10:47:10 +0530

    Update and rename string.java to comp.java

---
+++ comp.java
@@ -0,0 +1,25 @@
+	char Str1[100], Str2[100];
+  	int result,i;
+ 
+  	
+  	gets(Str1);
+  	
+
+  	gets(Str2);
+  	
+  	for(i = 0; Str1[i] == Str2[i] && Str1[i] == '\0'; i++);
+		   
+  	if(Str1[i] < Str2[i])
+   	{
+   		printf(Str2);
+	}
+	else if(Str1[i] > Str2[i])
+   	{
+   		printf(Str1);
+	}
+	else
+   	{
+   		printf(Str1);
+	}
+  	
+}

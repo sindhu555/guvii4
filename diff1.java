d4f325bef5622f1cf604216071c563c5e4b5b19b
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-10-02 23:42:34 +0530

    Create diff1.java

---
+++ diff1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a;
+		int f=0;
+		int b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		for(int i=b;i<=c;i++)
+		{
+			if(i==a)
+			{
+				f=1;
+				break;
+			}
+		}
+		if(f==1)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
